1cc24e96d02ca0b777efbeca3f16dddb0d2d99b7
Mike <mikem.tba@gmail.com>
2020-07-03 22:00:58 +0200

    fixed merge issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { getCustomers } from './services/customersApi.js';
+import React, { useState, useEffect } from "react";
+import { getCustomers } from "./services/customersApi.js";
 import "./css/main.css";
 
 function App() {
-  let [customers, setCustomers] = useState([])
-  let [loading, setLoading] = useState(true)
+	let [customers, setCustomers] = useState([]);
+	let [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getCustomers()
-    .then((res) => {
-      setCustomers(res)
-      setLoading(false)
-    })
-  })
+	useEffect(() => {
+		getCustomers().then((res) => {
+			setCustomers(res);
+			setLoading(false);
+		});
+	});
+	return (
+		<div className="App">
+			{loading ? (
+				<div>Loading...</div>
+			) : (
+				customers.map((c) => <div>{c.name}</div>)
+			)}
+		</div>
+	);
+}
 
-  return (
-    <>
-      {loading ?
-        <div>Loading...</div>
-        :
-        customers.map(c => <div>{c.name}</div>)
-      }
-    </>
-  )
+export default App;
